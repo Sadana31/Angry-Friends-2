1322158656f04e3804d40ff13412d7f6a90c6b9c
Sadana31 <63460528+Sadana31@users.noreply.github.com>
2020-05-09 11:55:51 +0530

    Add files via upload

---
+++ Pig2.js
@@ -0,0 +1,30 @@
+class Pig2 extends BaseClass {
+    constructor(x, y){
+      super(x,y,50,50);
+      this.image = loadImage("Buven.JPG");
+      this.Visiblity = 255;
+    }
+  
+   display(){
+    
+     if(this.body.speed < 3){
+      super.display();
+     }
+     else{
+       World.remove(world, this.body);
+       push();
+       this.Visiblity = this.Visiblity - 5;
+       tint(255,this.Visiblity);
+       image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+       pop();
+     }
+     
+   }
+  
+   score(){
+    if(this.body.speed>3){
+      score = score + 1;
+    }
+   }
+  
+  };
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Angry-Friends-2
-The final version of my game I have created using Visual Studio Code
+# AngryBirdsStage7
